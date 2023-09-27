526657498765482a734780ac5e380e943f84e035
Varun Sahni <66314478+notskamr@users.noreply.github.com>
2023-09-27 21:06:03 +0530

    Firebase Config

---
+++ src/firebase/client.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD_FSu5ASQKY5aOS2MlJ4ZO_0z2mxNRJhg",
+    authDomain: "parallelized-2f5be.firebaseapp.com",
+    projectId: "parallelized-2f5be",
+    storageBucket: "parallelized-2f5be.appspot.com",
+    messagingSenderId: "1051599211874",
+    appId: "1:1051599211874:web:d6eacc0b159ea7796b303b",
+    measurementId: "G-BDHE6RR412"
+};
+
+export const app = initializeApp(firebaseConfig);
