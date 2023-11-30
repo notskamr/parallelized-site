20b1ee74f6a2c2d9f58a6c53c9e501b0233a049b
Varun Sahni <sahnivarun62@gmail.com>
2023-11-30 14:07:28 +0530

    Add sitemap plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,25 +2,27 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import remarkToc from "remark-toc";
-
 import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      // Example: Disable injecting a basic `base.css` import on every page.
-      // Useful if you need to define and/or import your own custom `base.css`.
-      applyBaseStyles: false,
-    }),
-    mdx(),
-  ],
+  integrations: [tailwind({
+    // Example: Disable injecting a basic `base.css` import on every page.
+    // Useful if you need to define and/or import your own custom `base.css`.
+    applyBaseStyles: false
+  }), mdx(), sitemap()],
   build: {
-    redirects: false,
+    redirects: false
   },
   output: "hybrid",
   adapter: vercel(),
   markdown: {
-    remarkPlugins: [() => { return remarkToc({ skip: "Results" }) }],
-  },
+    remarkPlugins: [() => {
+      return remarkToc({
+        skip: "Results"
+      });
+    }]
+  }
 });
